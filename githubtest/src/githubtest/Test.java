e13e8f8a9d792b5a7897d79878453afb27b9a166
liguangyan666 <1723611292@qq.com>
2017-04-11 15:28:29 +0800

    输出一行

--- githubtest/src/githubtest/Test.java
+++ githubtest/src/githubtest/Test.java
@@ -1,12 +1,8 @@
 package githubtest;
 
-import org.omg.Messaging.SyncScopeHelper;
-
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("lgy");
 		System.out.println("sdfghjk");
-		System.out.println("dfghjk");
 	}
 
 }
